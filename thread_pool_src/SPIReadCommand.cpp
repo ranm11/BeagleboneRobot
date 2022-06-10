e23ecb88a84c24cfa324a4e945fe0583aff094ee
Ran Minervi <rminervy@synamedia.com>
2022-06-10 17:29:15 +0300

    add SPIReadCommand

---
+++ thread_pool_src/SPIReadCommand.cpp
@@ -0,0 +1,39 @@
+//#include "pch.h"
+#include "Command.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include "ThreadPool.h"
+#include<thread>
+#include<chrono>
+/*
+	this Function should read from UDP channel and enqueue Icommand to queue in an endless loop
+*/
+#define PORT     8080
+#define MAXLINE 1024
+
+SPIReadCommand::SPIReadCommand(ThreadPool & pool):m_Pool(pool),m_parseCommand(pool)
+{
+	
+}
+
+
+void SPIReadCommand::Execute()
+{
+	
+	while (true)
+	{
+		std::cout << "pending SPI packet" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+
+		char buffer[10];
+		int n(10);
+		//ICommand*  cmd = new ParseCommand(m_Pool, buffer, n);
+		//ICommand* engine = new EngineCommand(m_Pool, buffer);
+		//m_Pool.Enque(cmd);
+		m_parseCommand.Parse(buffer);
+	}
+	
+
+}
