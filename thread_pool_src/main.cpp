1638df4aee8e1e21aa03db753d5c5a398eb9bb88
Ran Minervy <rminervy@synamedia.com>
2023-03-29 15:59:18 +0300

    remove comments

--- thread_pool_src/main.cpp
+++ thread_pool_src/main.cpp
@@ -31,28 +31,6 @@ int main()
 	pool.Enque(spiChannel);
 	ICommand * rf24ReadCommand = new RF24ReadCommand(pool);
 	pool.Enque(rf24ReadCommand);
-	//for (int i = 0; i < 15; i++)
-	//{
-	//	ICommand * cmd = new TestCommand();
-	//	pool.Enque(cmd);
-	//}
-	//
-	//for (int i = 0; i < 15; i++)
-	//{
-	//	ICommand * cmd = new TestCommand();
-	//	pool.Enque(cmd);
-	//}
-
-	//_getch();
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
